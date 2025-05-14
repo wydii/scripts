ecc61976fef0ed3e6921c20a57f80e085585dd1b
yd <wydii@users.noreply.github.com>
2025-05-14 17:37:31 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 chmod +x hook-netbird
 chmod +x start-netbird
 
+echo "Removing existing hooks and scripts"
 rm /etc/initramfs-tools/hooks/hook-netbird
 rm /etc/initramfs-tools/scripts/init-premount/start-netbird
 
+echo "Copying new hooks and scripts"
 cp hook-netbird /etc/initramfs-tools/hooks/
 cp start-netbird /etc/initramfs-tools/scripts/init-premount/
 
+echo "Updating initramfs"
 update-initramfs -u
 
+echo "Done"
